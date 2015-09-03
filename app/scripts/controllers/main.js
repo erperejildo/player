b362d348c1455b807d9588fc895914aa7950dce2
Erperejildo <info@danielrodriguez.eu>
2015-09-03 21:22:48 +0200

    Load json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,29 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+
+  	/*$http.jsonp('https://githubusercontent.com/erperejildo/server/master/songs/songs.json', {
+	    params: {
+	      'callback': 'JSON_CALLBACK'
+	    }
+	}).success(function(response) {
+	    $scope.error = false;
+	    console.log('bien');
+	}).error(function(response) {
+	    $scope.error = true;
+	    console.log('error');
+	});*/
+
+	  $http.get('https://raw.githubusercontent.com/erperejildo/server/master/songs/songs.json').
+	  	then(function(response) {
+	  		$scope.songs = response.data;
+	  		$scope.current = 0;
+	  }, function(error) {
+	  });
+
+	  $scope.addFavourite = function() {
+	  	$scope.favourite = true;
+	  };
+
   });
