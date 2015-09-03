95219423652b3c34a3ee8473fad5fa23cfb14092
erperejido <danixiwaka@gmail.com>
2015-09-03 19:47:34 +0200

    First commit

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name playerApp
+ * @description
+ * # playerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('playerApp', [
+    'ngAnimate'
+  ]);
