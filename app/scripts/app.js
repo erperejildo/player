2e10d0da4325e907258550c829989f65ea10a54c
erperejido <danixiwaka@gmail.com>
2015-09-04 19:31:08 +0200

    Controller and directives

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,6 +9,34 @@
  * Main module of the application.
  */
 angular
-  .module('playerApp', [
-    'ngAnimate'
-  ]);
+	.module('playerApp', [
+		'ngAnimate',
+		'ngSanitize'
+	])
+	// when load element
+	.directive('audioplayer', function() {
+		return {
+			scope: {
+				updateTime: '&playing'
+			},
+			link: function(scope, element, attrs) {
+				element.bind('timeupdate', function(evt) {
+					scope.updateTime();
+				});
+			}
+		};
+	})
+	// get duration when song loaded
+	.directive('elemReady', function($parse) {
+		return {
+			restrict: 'A',
+			link: function($scope, elem, attrs) {
+				elem.ready(function() {
+					$scope.$apply(function() {
+						var func = $parse(attrs.elemReady);
+						func($scope);
+					})
+				})
+			}
+		}
+	});
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,29 +8,109 @@
  * Controller of the playerApp
  */
 angular.module('playerApp')
-  .controller('MainCtrl', function ($scope, $http) {
+	.controller('MainCtrl', function($scope, $http) {
 
-  	/*$http.jsonp('https://githubusercontent.com/erperejildo/server/master/songs/songs.json', {
-	    params: {
-	      'callback': 'JSON_CALLBACK'
-	    }
-	}).success(function(response) {
-	    $scope.error = false;
-	    console.log('bien');
-	}).error(function(response) {
-	    $scope.error = true;
-	    console.log('error');
-	});*/
+		$scope.playing = false;
+		$scope.imgLoaded = false;
+		$scope.seconds = '0:00';
+		$scope.muted = false;
+		$scope.url = 'https://rawgit.com/erperejildo/server/master/songs/';
 
-	  $http.get('https://raw.githubusercontent.com/erperejildo/server/master/songs/songs.json').
-	  	then(function(response) {
-	  		$scope.songs = response.data;
-	  		$scope.current = 0;
-	  }, function(error) {
-	  });
+		$http.get('https://raw.githubusercontent.com/erperejildo/server/master/songs/songs.json').
+		then(function(response) {
+			$scope.songs = response.data;
+			$scope.current = 0;
+			$scope.activeSong = document.getElementById('song');
+			$scope.activeSong.src = $scope.url + $scope.songs[$scope.current].url;
+		}, function(error) {});
 
-	  $scope.addFavourite = function() {
-	  	$scope.favourite = true;
-	  };
+		$scope.showImg = function() {
+			$scope.imgLoaded = true;
+		};
 
-  });
+		$scope.addFavourite = function() {
+			$scope.favourite = true;
+		};
+
+		$scope.playPause = function() {
+			if ($scope.activeSong.paused) {
+				$scope.playing = true;
+				$scope.activeSong.play();
+			} else {
+				$scope.playing = false;
+				$scope.activeSong.pause();
+			}
+		}
+
+		$scope.mute = function() {
+			if ($scope.muted) {
+				$scope.muted = false;
+				$scope.activeSong.volume = $scope.volume_;
+			} else {
+				$scope.muted = true;
+				$scope.volume_ = $scope.activeSong.volume;
+				$scope.activeSong.volume = 0;
+			}
+		};
+
+		$scope.updateTime = function(prueba) {
+			var currentSeconds = (Math.floor($scope.activeSong.currentTime % 60) < 10 ? '0' : '') + Math.floor($scope.activeSong.currentTime % 60);
+			var currentMinutes = Math.floor($scope.activeSong.currentTime / 60);
+
+			$scope.$apply(function(){
+				$scope.seconds = currentMinutes + ":" + currentSeconds;
+			});
+
+			var percentageOfSong = ($scope.activeSong.currentTime / $scope.activeSong.duration);
+			var percentageOfSlider = document.getElementById('progress').offsetWidth * percentageOfSong;
+
+			$scope.barWidth = {
+				'width': Math.round(percentageOfSlider) + 'px'
+			};
+			console.log($scope.barWidth);
+		}
+
+		$scope.changeVolume = function (number, direction) {
+			$scope.muted = false;
+			$scope.activeSong.volume = $scope.volume_;
+			if ($scope.activeSong.volume > 0 && direction == 'less') {
+				$scope.activeSong.volume = ($scope.activeSong.volume - (number / 100)).toFixed(2);
+			}
+			if ($scope.activeSong.volume < 1 && direction == 'more') {
+				$scope.activeSong.volume = ($scope.activeSong.volume + (number / 100)).toFixed(2);
+			}
+		}
+
+		$scope.songPosition = function(e) {
+			var barWidth = document.getElementById('progress').offsetWidth;
+			var clickPosition = e.offsetX
+
+			var percentage = (e.offsetX / barWidth);
+
+			$scope.activeSong.currentTime = $scope.activeSong.duration * percentage;
+		};
+
+		$scope.changeSong = function(val) {
+			$scope.activeSong.pause();
+			$scope.playing = false;
+			if (val == 'prev') {
+				if ($scope.current == 0) {
+					$scope.current = $scope.songs.length - 1;
+				} else {
+					$scope.current --;
+				}
+			} else {
+				if ($scope.current + 1 == $scope.songs.length) {
+					$scope.current = 0;
+				} else {
+					$scope.current ++;
+				}
+			}
+			$scope.activeSong.src = $scope.url + $scope.songs[$scope.current].url;
+			$scope.barWidth = {
+				'width': '0px'
+			};
+			$scope.seconds = '0:00';
+		};
+
+	});
